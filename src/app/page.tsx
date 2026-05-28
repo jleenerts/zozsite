165df70925b60f1570fa20e98c668da4ef8c4038
Jacob Leenerts <jacobleenerts97+code@gmail.com>
2026-05-28 01:44:24 -0700

    update layout some more

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,14 @@
 // following the guide at https://nextjs.org/learn/react-foundations/
 
 import Link from "next/link";
-import { LikeButton } from "./user";
 import styles from "./index.module.css";
 let sy = styles;
 
 export default function HomePage() {
-  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
-
   return (
     <div className={`${sy.index}`}>
       <div className={`${sy.index}`}>
         <Link className={`buttons`} href={`./pages`}>Check out my apps!</Link>
-      </div>
-      <div className={`${sy.index}`}>
-        <LikeButton className={`buttons`}/>
       </div>
     </div>
   );
