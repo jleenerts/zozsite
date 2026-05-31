f98a17cd788736fba93277f2cb09dd95d206f06c
Jacob Leenerts <jacobleenerts97+code@gmail.com>
2026-05-31 04:35:03 -0700

    add Heroicons to package list and fix project hierarchy for proper image importing

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,10 @@
 'use client';
 
 import Link from "next/link";
+import Image from "next/image";
 import { usePathname } from "next/navigation";
+
+import { LightBulbIcon } from "@heroicons/react/24/outline";
 
 // toggle the site dark mode
 export function ColorButton({ className }: { className?: string }) {
@@ -19,7 +22,8 @@ export function ColorButton({ className }: { className?: string }) {
   
   return (
     <button onClick={handleClick} className={className}>
-    Toggle Colors</button>
+      <LightBulbIcon className="w-10" />
+    </button>
   );
 }
 
@@ -40,9 +44,18 @@ export default function Header() {
       <div className={`basis-96 py-2 text-right bg-gray-500`}>
         <div className="flex flex-col gap-2 [&>*]:gap-2">
           
-          <div className="bg-gray-400 mr-2 rounded-r-md">
-            <Link href="/" className="font-semibold text-xl pr-2 hover:text-orange-500 duration-150">
-              Jacob's Site
+          <div className="bg-gray-400 mr-1 rounded-r-md hover:text-orange-500 hover:duration-150">
+            <Link href="/about" replace
+                  className="grid grid-flow-col justify-items-end pr-2">
+              <Image
+                src="/zoz-logo-big.png"
+                height={1024}
+                width={1024}
+                objectPosition="relative"
+                alt="Logo designed by Zoz, a circular and stylized orange capital Z against a black background."
+                className="w-10 justify-right"
+              />
+              <div className="place-content-center font-semibold text-right">Jacob's Site</div>
             </Link>
           </div>
           
@@ -51,7 +64,7 @@ export default function Header() {
               <Link
                 key={item.href}
                 href={item.href}
-                className={`hover:text-orange-500 mr-2 pr-2 bg-gray-400 rounded-r-md border-black`}
+                className={`hover:text-orange-500 hover:duration-150 mr-1 pr-2 bg-gray-400 rounded-r-md border-black`}
               >
                 {item.label}
               </Link>
@@ -62,8 +75,8 @@ export default function Header() {
         
         
       </div>
-      <div className={`basis-4 bg-linear-to-r from-gray-500 to-white/100`}></div>
-      <ColorButton className="border rounded-md bg-gray-200 dark:bg-gray-700 absolute bottom-2 right-3"/>
+      <div className={`basis-4 bg-linear-to-r from-gray-500 to-white dark:to-black`}></div>
+      <ColorButton className="p-3 bg-gray-500 absolute bottom-2 right-3"/>
     </div>
   );
 }
