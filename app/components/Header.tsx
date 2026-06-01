c6141415bf6f82bd538a11956e8b81311b95e168
Jacob Leenerts <jacobleenerts97+code@gmail.com>
2026-05-31 19:03:07 -0700

    finally got sidebar toggle to work LFGGGGGGG

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -22,7 +22,7 @@ export function ColorButton({ className }: { className?: string }) {
   
   return (
     <button onClick={switchThemes} className={className}>
-      <LightBulbIcon className="w-10"/>
+      <LightBulbIcon className=""/>
     </button>
   );
 }
@@ -31,12 +31,12 @@ export function ToggleSidebarButton({ className }: { className?: string }) {
   const sidebar = document.getElementById('sidebar');
 
   function toggleSidebar() {
-    console.log("toggle");
+    sidebar?.classList.toggle('-translate-x-full');
   }
 
   return (
     <button onClick={toggleSidebar} className={className}>
-      <Bars3Icon className="w-10"/>
+      <Bars3Icon className=""/>
     </button>
   );
 }
@@ -52,55 +52,71 @@ export default function Header() {
   ];
 
   return (
-    <aside className="fixed left-0
-                    h-screen max-w-1/10 min-w-3xs
-                    flex">
+    <aside>
       
-      <div className={`sidebar basis-96 py-2
-                       text-right bg-gray-500 duration-300`}>
-        
-        <div className="flex flex-col gap-2 [&>*]:gap-2">
-          
-          <div className="bg-gray-400 mr-1 rounded-r-md hover:text-orange-500 hover:duration-150">
-            <Link href="/about" replace
-                  className="flex justify-end pr-2">
-              <div className="w-fill">
-                <Image
-                  src="/zoz-logo-sm.png"
-                  height={1024}
-                  width={1024}
-                  alt="Logo designed by Zoz, a circular and stylized orange capital Z against a black background."
-                  className="w-10"
-                />
-              </div>
-              <div className="pl-2 place-content-center font-semibold">Jacob's Site</div>
-            </Link>
+      <div id="sidebar"
+           className="fixed left-0
+                      h-screen max-w-1/10 min-w-3xs
+                      flex
+                      transition-all duration-300 ease-in-out transform"
+      >
+        <div 
+          className="basis-96 py-2
+                     text-right bg-gray-500"
+        >  
+          <div 
+            className="flex flex-col gap-2 [&>*]:gap-2"
+          >
+            
+            <div
+              className="mr-1
+                         hover:text-orange-500 hover:duration-150"
+            >
+              <Link href="/about"
+                    className="flex justify-end pr-2"
+              >
+                <div>
+                  <Image
+                    src="/zoz-logo-sm.png"
+                    height={1024}
+                    width={1024}
+                    alt="Logo designed by Zoz, a circular and stylized orange capital Z against a black background."
+                    className="w-10"
+                  />
+                </div>
+                <div className="pl-2 place-content-center font-semibold">Jacob's Site</div>
+              </Link>
+            </div>
+            
+            <nav className="flex flex-col">
+              {navItems.map((item) => (
+                <Link
+                  key={item.href}
+                  href={item.href}
+                  className={`mr-1 pr-2
+                              bg-gray-400 rounded-r-md border-black
+                              hover:text-orange-500 hover:duration-150`}
+                >
+                  {item.label}
+                </Link>
+              ))}
+            </nav>
+
           </div>
           
-          <nav className="flex flex-col">
-            {navItems.map((item) => (
-              <Link
-                key={item.href}
-                href={item.href}
-                className={`mr-1 pr-2
-                            bg-gray-400 rounded-r-md border-black
-                            hover:text-orange-500 hover:duration-150`}
-              >
-                {item.label}
-              </Link>
-            ))}
-          </nav>
-
+          
         </div>
+        <div
+          className="basis-4 bg-linear-to-r from-gray-500 to-white dark:to-black"
+        />
         
-        
+        <ColorButton className="absolute bottom-2 right-3 p-3 w-18
+                                cursor-pointer dark:text-black not-dark:text-yellow-100 duration-80"/>
       </div>
-      <div className={`basis-4 bg-linear-to-r from-gray-500 to-white dark:to-black`}></div>
-      <Bars3Icon className="absolute top-2 left-2 h-10
-                            cursor-pointer text-black "/>
-      <ColorButton className="cursor-pointer p-3
-                              absolute bottom-2 right-3
-                              dark:text-black not-dark:text-yellow-100 duration-80"/>
+      <ToggleSidebarButton
+        className="absolute top-2 left-2 w-10
+                   cursor-pointer text-black "
+      />
     </aside>
   );
 }
