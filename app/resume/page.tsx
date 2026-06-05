fc29e39d307eb01dd596c8ecd5e047044cb3409f
Jacob Leenerts <jacobleenerts97+code@gmail.com>
2026-06-05 05:02:10 -0700

    add font

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,13 +1,21 @@
 import type { Metadata } from 'next';
 
+import { IBM_Plex_Sans_Thai_Looped } from "next/font/google";
+
 export const metadata: Metadata = {
   title: `Jacob's Resume`,
   description: `Check out my coding projects & experience, from web apps to AR experimental research!`,
 }
 
+const ibmPlexThai = IBM_Plex_Sans_Thai_Looped({
+   variable: "--font-ibm-plex-sans-thai",
+   subsets: ["latin"],
+   weight: "400",
+});
+
 export default function ResumePage() {
    return(
-      <div>
+      <div className={`${ibmPlexThai.className}`}>
          <section id="summary" className="">
             <div className="">
                <h1>Jacob Leenerts</h1>
