39e6b1bcf66da1ff48ed2575cfe326b024272675
Jacob Leenerts <jacobleenerts97+code@gmail.com>
2026-06-05 09:29:17 -0700

    can't get image right-aligned but I'm tired af

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import type { Metadata } from 'next';
 
 import { IBM_Plex_Sans_Thai_Looped } from "next/font/google";
@@ -17,8 +18,20 @@ export default function ResumePage() {
    return(
       <div className={`${ibmPlexThai.className}`}>
          <section id="summary" className="">
-            <div className="">
-               <h1>Jacob Leenerts</h1>
+            <div className="flex">
+               <div className="flex flex-col">
+                  <h1 className="my-4 text-4xl">Jacob Leenerts</h1>
+                  <p className="text-4xl border-b w-max">Software Developer</p>
+               </div>
+               
+               <Image src="/jacob-forest-cropped.jpg"
+                           height={2316}
+                           width={1656}
+                           fill={false}
+                           loading={`eager`}
+                           alt="A selfie I took at the UCSC campus. I am half-shaded and behind me is foliage with tall trees."
+                           className="inset-y-0 right-0 w-1/4"
+               />
             </div>
          </section>
          <section id="highlights" className="">
